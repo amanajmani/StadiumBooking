9cd215c4ee6ccad768bd7dbadcc3c15f8f34505a
amanajmani <aman.ajmaani@gmail.com>
2020-02-13 00:57:00 +0530

    Add Navigation

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 /**
  * @format
  */
-
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
 import Home from './src/app/containers/App';
 import {name as appName} from './app.json';
 import store from './src/config/store';
 
 class App extends Component {
   render() {
-    return (
-      <Provider store={store}>
+    return (      
+    <Provider store={store}>
+      <NavigationContainer>
         <Home />
-      </Provider>
+      </NavigationContainer>
+    </Provider>
     );
   }
 }
