3da863b64314c37b8f103b895d014f3008a81490
amanajmani <aman.ajmaani@gmail.com>
2020-02-13 14:59:19 +0530

    [Home] moving data to constants

--- src/app/containers/Home.js
+++ src/app/containers/Home.js
@@ -9,13 +9,8 @@ import {
 import { Chip, Card, FAB } from 'react-native-paper';
 import CalendarStrip from 'react-native-calendar-strip';
 
+import {timeDetails, court} from '../utils/constants';
 import styles from '../assets/styles/Home';
-
-const court = ['court 1', 'court 2', 'court 3', 'court 4', 'court 5'];
-
-const timeDetails = ['06:00 AM', '07:00 AM', '08:00 AM',
-'09:00 AM','10:00 AM','11:00 AM','12:00 PM',
-'01:00 PM','02:00 PM','03:00 PM','04:00 PM'];
 
 function Home(props) {
   const [isSelected, setIsSelected] = useState({});
---
+++ src/app/utils/constants.js
@@ -0,0 +1,5 @@
+export const court = ['court 1', 'court 2', 'court 3', 'court 4', 'court 5'];
+
+export const timeDetails = ['06:00 AM', '07:00 AM', '08:00 AM',
+'09:00 AM','10:00 AM','11:00 AM','12:00 PM',
+'01:00 PM','02:00 PM','03:00 PM','04:00 PM'];
